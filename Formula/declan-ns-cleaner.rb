d91498478fb1acb36788dba8700c43f23c1696e4
Declan <103009135+Rookie0031@users.noreply.github.com>
2024-09-27 15:37:15 +0900

    add description

--- Formula/declan-ns-cleaner.rb
+++ Formula/declan-ns-cleaner.rb
@@ -1,5 +1,5 @@
-class GoCli < Formula
-  desc "Your CLI tool description"
+class DeclanNscleaner < Formula
+  desc "Check and Clean useless namespace deployed in kubernetes cluster"
   homepage "https://github.com/Rookie0031/go-cli"
   url "https://github.com/Rookie0031/declan-cli/releases/download/v0.1.0/declan-ns-cleaner_0.1.0_darwin_arm64.tar.gz_0.1.0_darwin_amd64.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
